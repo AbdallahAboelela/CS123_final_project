15cdf47b0420d9da6ffcb2ffe1eff20ea8c22abd
Abdallah Aboelela <akaboelela@cs.uchicago.edu>
2019-05-12 17:57:36 -0500

    CONVERSION WORKING

--- converting_to_csvs/convert.py
+++ converting_to_csvs/convert.py
@@ -5,20 +5,23 @@ from google.cloud import bigquery
 import os
 
 def convert_to_csv():
-    client = bigquery.Client()
+    client = bigquery.Client(project='kasa-238922')
 
     dataset_prefix = '`bigquery-public-data.new_york.tlc_yellow_trips_'
     year = 2009
 
     while year <= 2016:
-        sql = 'SELECT * FROM ' + dataset_prefix + str(year) + '` LIMIT 1000;'
+        sql = 'SELECT * FROM ' + dataset_prefix + str(year) + '`;'
 
+        print(str(year) + " Requesting...")
         trips = client.query(sql).to_dataframe()
 
         fname = str(year) + '_trips.csv'
 
+        print(str(year) + " Writing csv...")
         trips.to_csv(fname)
 
+        print(str(year) + " Uploading...")
         os.system("gsutil cp " + "./" + fname + " gs://kasa_nyc_taxi_data/NYC_taxi_2009-2016.csv/")
         os.system("rm ./" + fname)
         
