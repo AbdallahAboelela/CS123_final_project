a81647012b5ebd7ff0a71cc9916be504486d9e58
Abdallah Aboelela <abdallahaboelela@github.com>
2019-05-13 18:38:20 -0500

    progress on mr_trips, issues with dataproc

--- dataproc_work/mr_trips.py
+++ dataproc_work/mr_trips.py
@@ -9,25 +9,30 @@ import pickle
 
 class MRNodeTime(MRJob):
     def mapper_init(self):
-        self.G = pickle.load(open('G_adj.p', 'rb'))
+        self.G = pickle.load(open('/Users/abdallahaboelela/Documents/GitHub/'
+            'CS123_final_project/dataproc_work/G_adj.p', 'rb'))
 
     def mapper(self, _, line):
+
         l = line.split(',')
         d_lat, d_long = l[2:4]
         p_lat, p_long = l[13:15]
 
-        paths, times = boundaries.get_path_time(self.G, (p_lat, p_long), (d_lat, d_long))
-        
-        for i, nodes in enumerate(paths):
-            time = times[i]
-            yield (min(nodes), max(nodes)), time
+        try:
+            paths, times = boundaries.get_path_time(self.G, (p_lat, p_long), (d_lat, d_long))
+
+            for i, nodes in enumerate(paths):
+                time = times[i]
+                yield (min(nodes), max(nodes)), time
+
+        except Exception as e:
+            pass
+
+    def combiner_init(self):
+        print("Reached combiner stage")
 
     def combiner(self, path, times):
         yield path, sum(times)
-
-    def reducer_init(self):
-        cols = ['n1', 'n2', 'total_time']
-        self.node_times = pd.DataFrame(columns = cols)
 
     def reducer(self, path, times):
         yield path, sum(times)
