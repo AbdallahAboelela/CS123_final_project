7520e6f3a25e522b435706a3f02b31dc8ff9abea
Abdallah Aboelela <abdallahaboelela@github.com>
2019-05-13 17:15:27 -0500

    Reorganizing before mrjob attempt

--- osmnx_work/boundaries.py
+++ osmnx_work/boundaries.py
@@ -18,7 +18,8 @@ def construct_G_adj():
 
     speed_lims = gpd.read_file('speed_lims.geojson')
     speed_lims.drop(['shape_leng', 'postvz_sg', 'street'], axis=1, inplace=True)
-    speed_lims['postvz_sl'][speed_lims['postvz_sl'] == '0'] = 25
+    speed_lims['postvz_sl'] = speed_lims['postvz_sl'].astype(int)
+    speed_lims['postvz_sl'][speed_lims['postvz_sl'] == 0] = 25
 
     speed_lims_bounds = speed_lims['geometry'].bounds
     speed_lims['u'] = ox.get_nearest_nodes(G, speed_lims_bounds['minx'],\
@@ -37,23 +38,33 @@ def construct_G_adj():
     edges_proj['A'] = edges_proj['u'].map(str) + ' ' + edges_proj['v'].astype(str)
 
     ### merge the data frame    
-    merged_df_1 = edges_proj.merge(speed_lims, how='left', on='A')
-    merged_df_2 = edges_proj.merge(speed_lims, how='left', left_on=['A'], right_on=['B'])
-    merged_df = merged_df_1.merge(merged_df_2, left_on=['A'], right_on=['A_x'])
-    ##
+    merged_df_1 = edges_proj.merge(speed_lims[['postvz_sl', 'A']], how='left', on=['A'])
+    merged_df_2 = edges_proj.merge(speed_lims[['postvz_sl', 'B']], how='left', left_on=['A'], right_on=['B'])
+    
+    merged_df = merged_df_1.merge(merged_df_2[['postvz_sl', 'A']], on=['A'])
+    merged_df.drop('A', axis=1, inplace=True)
 
-    merged_df['time'] = merged_df['length'] / merged_df['postvz_sl']
-    G_adj = ox.save_load.gdfs_to_graph(nodes_df, merged_df)  # we can save this and call this when need it in function 
-    pickle.dump(com_Gs, open('adj_G.p', 'wb'))
+    nan_speed = pd.isnull(merged_df['postvz_sl_x'])
+    merged_df['postvz_sl_x'][nan_speed] = merged_df['postvz_sl_y'][nan_speed]
+    merged_df.drop('postvz_sl_y', axis=1, inplace=True)
+    nan_speed_2 = pd.isnull(merged_df['postvz_sl_x'])
+    merged_df['postvz_sl_x'][nan_speed_2] = 25
+
+    ##
+    # 'length' is in meters so need to convert to miles 
+    merged_df['postvz_sl_x'] *= 1609.34
+    print(merged_df.columns)
+    merged_df['time'] = merged_df['length'] / merged_df['postvz_sl_x']
+    merged_df.drop(['postvz_sl_x'], axis=1, inplace=True)
+    G_adj = ox.save_load.gdfs_to_graph(nodes_proj, merged_df)  # we can save this and call this when need it in function 
+    pickle.dump(G_adj, open('G_adj.p', 'wb'))
 
 
 def get_path_time(G, curr_loc, dest_loc):
 
-    pickle.load(open('adj_G.p', 'rb'))
-
     orig_node = ox.get_nearest_node(G, curr_loc, method='euclidean')
     target_node = ox.get_nearest_node(G, dest_loc, method='euclidean')
-    route = nx.shortest_path(G_adj, source=orig_node, target=target_node, weight='time')
+    route = nx.shortest_path(G, source=orig_node, target=target_node, weight='time')
     
     nodes_proj, edges_proj = ox.graph_to_gdfs(G, nodes=True, edges=True)
 
@@ -66,9 +77,3 @@ def get_path_time(G, curr_loc, dest_loc):
         times.append(time)
 
     return pairs, times 
-
-
-
-
-
-    
