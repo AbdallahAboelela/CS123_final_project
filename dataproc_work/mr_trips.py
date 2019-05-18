0044bc94ee9631d8f7de6406e6af510665b70d1c
Adam Alexander Oppenheimer <adam.a.oppenheimer@gmail.com>
2019-05-17 20:20:01 -0500

    almost working?

--- dataproc_work/mr_trips.py
+++ dataproc_work/mr_trips.py
@@ -8,15 +8,17 @@
 # { time python3 mr_trips.py duper_short.csv > test_write.csv ; } 2> time.txt
 
 import os
-os.system("pip3 install --user --upgrade pip")
-os.system("pip3 install --user pandas")
-os.system("pip3 install --user mrjob")
+os.system("sudo -H pip3 install --upgrade pip")
+os.system("sudo -H pip3 install pandas")
+os.system("sudo -H pip3 install mrjob")
+os.system("xcode-select --install")
+os.system('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')
 os.system("brew install spatialindex")
-os.system("pip3 uninstall osmnx")
-os.system("pip3 install --user osmnx")
+#os.system("pip3 uninstall osmnx")
+os.system("sudo -H pip3 install osmnx")
 os.system("sudo apt install python3-rtree")
-os.system("pip3 uninstall networkx")
-os.system("pip3 install --user networkx")
+#os.system("pip3 uninstall networkx")
+os.system("sudo -H pip3 install networkx")
 
 from mrjob.job import MRJob
 import pandas as pd
