4ecf0d86fd8d7438dc0a2376e5f3e446f110ad18
Abdallah Aboelela <akaboelela@cs.uchicago.edu>
2019-05-26 15:36:23 -0500

    minor fixes

--- dataproc_work/extract_csv.py
+++ dataproc_work/extract_csv.py
@@ -14,22 +14,23 @@ def extract_files(date1, date2):
     date1 = '01-04'
     date2 = '01-10'
     '''
-    date1 = datetime.strptime('2019-' + date1, '%Y-%m-%d %H:%M:%S')
-    date2 = datetime.strptime('2019-' + date2, '%Y-%m-%d %H:%M:%S')
+    date1 = datetime.strptime('2020-' + date1, '%Y-%m-%d %H:%M:%S')
+    date2 = datetime.strptime('2020-' + date2, '%Y-%m-%d %H:%M:%S')
 
     dir_name = str(date1).strip(' ')[4:] + '_' + str(date2).strip(' ')[4:] #4 to skip year
     df = pd.read_csv(FILE_NAME, infer_datetime_format=True, parse_dates=['first_trip_dt', 'last_trip_dt'])
-
-    df.first_trip_dt = df.first_trip_dt.apply(lambda x: x.year = 2019)
-    df.last_trip_dt = df.last_trip_dt.apply(lambda x: x.year = 2019)
+ 
+    df.first_trip_dt = df.first_trip_dt.apply(lambda dt: dt.replace(year = 2020))
+    df.last_trip_dt = df.last_trip_dt.apply(lambda dt: dt.replace(year = 2020))
     # , date_parser=date_parse)
 
+    os.system('mkdir relevant_csvs')
     for index, row in df.iterrows():
         if (row['first_trip_dt'] <= date1 <= row['last_trip_dt']) or\
                 (row['first_trip_dt'] <= date2 <= row['last_trip_dt']):
             full_name, str_num = get_fname(row['file_num'])
-            os.system('mkdir {}'.format(dir_name))
-            os.system('gsutil cp {} {}/{}'.format(full_name, dir_name, str_num))
+
+            os.system('gsutil cp {} relevant_csvs/'.format(full_name))
 
 def get_fname(num):
     n = len(str(num))
